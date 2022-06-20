02d80a4b6cd99b679c3ee775f68e71830848257e
Piotr Deregowski <deregowskipiotr@interia.pl>
2022-06-20 20:10:42 +0200

    Add new file

--- js/script.js
+++ js/script.js
@@ -10,9 +10,7 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
 
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -20,9 +18,26 @@ console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'kamień';
 } 
-else if (randomNumber=='2') {computerMove='papier'}
-else if (randomNumber=='3') {computerMove='nożyce'}
+else if (randomNumber=='2') {computerMove='papier';}
+else if (randomNumber=='3') {computerMove='nożyce';}
 else {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerMove = 'kamień';
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} 
+
+else if (playerInput == '2') { playerMove = 'nożyce';}
+else if (playerInput == '3') { playerMove = 'papier';}
+
+else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
